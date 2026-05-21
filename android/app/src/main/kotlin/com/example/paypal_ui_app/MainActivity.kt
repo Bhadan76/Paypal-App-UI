801183442b662ed3158f8142ca6c1a503d08796c
Bhadan <bhadanpaul@gmail.com>
2026-05-21 21:56:33 +0600

    Paypal UI

---
+++ android/app/src/main/kotlin/com/example/paypal_ui_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.paypal_ui_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Paypal App',
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+
+    );
+  }
+}
